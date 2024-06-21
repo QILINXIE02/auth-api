7fca4ec5603bf40a976728fc4f19226145661ed0
qilinxie02 <qxie@gicw.org>
2024-06-21 15:00:36 -0700

    prepares flower app datas

--- src/models/data-collection.js
+++ src/models/data-collection.js
@@ -1,21 +1,17 @@
 'use strict';
 
-// THIS IS THE STRETCH GOAL ...
-// It takes in a schema in the constructor and uses that instead of every collection
-// being the same and requiring their own schema. That's not very DRY!
-
 class DataCollection {
-
   constructor(model) {
     this.model = model;
   }
 
-  get(id) {
+  async get(id) {
     if (id) {
-      return this.model.findOne({ id });
-    }
-    else {
-      return this.model.findAll({});
+      const record = await this.model.findOne({ where: { id } });
+      return { count: record ? 1 : 0, results: record ? [record] : [] };
+    } else {
+      const records = await this.model.findAll({});
+      return { count: records.length, results: records };
     }
   }
 
@@ -23,15 +19,18 @@ class DataCollection {
     return this.model.create(record);
   }
 
-  update(id, data) {
-    return this.model.findOne({ where: { id } })
-      .then(record => record.update(data));
+  async update(id, data) {
+    const record = await this.model.findOne({ where: { id } });
+    if (record) {
+      await record.update(data);
+      return record;
+    }
+    return null;
   }
 
   delete(id) {
-    return this.model.destroy({ where: { id }});
+    return this.model.destroy({ where: { id } });
   }
-
 }
 
 module.exports = DataCollection;
--- src/models/store.js
+++ src/models/store.js
@@ -2,10 +2,8 @@
 
 const storeModel = (sequelize, DataTypes) => sequelize.define('store', {
   name: { type: DataTypes.STRING, required: true },
-  categories: { type: DataTypes.STRING, required: true },
-  instock: { type: DataTypes.STRING, required: true },
-  price: { type: DataTypes.STRING, required: true },
-  __V: { type: DataTypes.STRING, required: true }
+  colors: { type: DataTypes.STRING, required: true },
+  image: { type: DataTypes.STRING, required: true }
 });
 
 module.exports = storeModel;
